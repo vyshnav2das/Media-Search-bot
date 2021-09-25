28bf0d2b1cc1100e35a1107d2438db1d013a7101
Iamsreerag <89203874+Iamsreerag@users.noreply.github.com>
2021-09-26 01:12:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -196,4 +196,4 @@ async def bot_info(bot, message):
             InlineKeyboardButton('📺𝐖𝐞𝐛𝐬𝐞𝐫𝐢𝐞𝐬📺', url='https://t.me/netflixorgi')
         ]
         ]
-    await message.reply(text="<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="<b>Group : <a href='https://t.me/malayayalies'>Group</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nChannel : <a href='https://t.me/malayalam_movies_h'>Click here</a>\nUpdate Channel : <a href='https://t.me/malayalam_movies_h'>channel</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
